cb30393f2986e13bee04ba519b5e2f41acddc3c9
AntoineSirot <sirot_antoine@bbox.fr>
2024-01-19 13:54:14 +0100

    feat: Adding comments

--- src/VaultContract.sol
+++ src/VaultContract.sol
@@ -5,9 +5,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
-contract GHOStakingVault is ERC4626 {
-    ERC20 public immutable GHO;
+/**
+ * @title GHO Staking Vault
+ * @dev This contract implements a staking vault for the GHO token.
+ * @notice Allows users to participate in games and receive rewards based on their performance.
+ * @author LFRaclette
+ */
 
+contract GHOStakingVault is ERC4626 {
+    /**
+     * @dev Structure to represent a game.
+     */
     struct Game {
         uint id;
         string gameName;
@@ -19,14 +27,17 @@ contract GHOStakingVault is ERC4626 {
         bool finished;
     }
 
+    ERC20 public immutable GHO;
     Game[] public games;
     address public superAdmin;
 
+    // Event declarations
     event GameCreated(uint gameId);
     event GameFinished(uint gameId);
     event GameStarted(uint gameId);
     event ParticipantEnteredGame(uint gameId, address participant);
 
+    // Error declarations
     error NotEnoughFGHO(uint amountRequire);
     error IncorrectId(uint gameId);
     error NotStartedGame(uint gameId);
@@ -38,6 +49,9 @@ contract GHOStakingVault is ERC4626 {
     error IncorrectRanking(uint sendLength, uint wantedLength);
     error AlreadyPlaying(address userAddress);
 
+    /**
+     * @dev Modifier to restrict functions to the super admin only.
+     */
     modifier onlySuperAdmin() {
         if (msg.sender != superAdmin) {
             revert NotSuperAdmin(msg.sender, superAdmin);
@@ -45,6 +59,12 @@ contract GHOStakingVault is ERC4626 {
         _;
     }
 
+    /**
+     * @dev Constructor for the GHOStakingVault contract.
+     * @param _GHO Address of the GHO token contract.
+     * @param name_ Name of the staking vault token.
+     * @param symbol_ Symbol of the staking vault token.
+     */
     constructor(
         ERC20 _GHO,
         string memory name_,
@@ -54,6 +74,11 @@ contract GHOStakingVault is ERC4626 {
         GHO = _GHO;
     }
 
+    /**
+     * @notice Fetches details of a specific game.
+     * @param gameId The ID of the game to retrieve.
+     * @return Tuple containing game details.
+     */
     function getGame(
         uint gameId
     )
@@ -83,6 +108,13 @@ contract GHOStakingVault is ERC4626 {
         );
     }
 
+    /**
+     * @notice Creates a new game with the specified parameters.
+     * @dev Emits a GameCreated event on successful creation.
+     * @param gameName Name of the game played.
+     * @param totalPrice Total prize pool of the game.
+     * @param repartition Array representing the prize distribution.
+     */
     function createGame(
         string memory gameName,
         uint totalPrice,
@@ -115,6 +147,11 @@ contract GHOStakingVault is ERC4626 {
         emit GameCreated(gameId);
     }
 
+    /**
+     * @notice Allows a user to enter an existing game.
+     * @dev Checks for game validity and participant's eligibility.
+     * @param gameId The ID of the game to enter.
+     */
     function enterGame(uint gameId) public {
         if (gameId >= games.length) {
             revert IncorrectId(gameId);
@@ -151,13 +188,19 @@ contract GHOStakingVault is ERC4626 {
         }
     }
 
-    function distributeRewards(
-        uint gameId,
-        address[] memory ranking
-    ) public onlySuperAdmin {
+    /**
+     * @notice Distributes the rewards of a game to the participants based on their ranking.
+     * @dev In reality, it sould use the superAdmin modifier so only the backend could distribute the rewards results. Ensures the game is started and not finished.
+     * @param gameId The ID of the game for which to distribute rewards.
+     * @param ranking Array of addresses representing the participants in their finishing order.
+     */
+    function distributeRewards(uint gameId, address[] memory ranking) public {
         Game memory currentGame = games[gameId];
         if (!currentGame.started) {
             revert NotStartedGame(gameId);
+        }
+        if (currentGame.finished) {
+            revert IsOverGame(gameId);
         }
         if (currentGame.numberOfPlayer != ranking.length) {
             revert IncorrectRanking(ranking.length, currentGame.numberOfPlayer);
